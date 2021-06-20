5ee8dd4821096508b63e9b9c36a7ddfd42278b1f
helwet <39155763+helwet@users.noreply.github.com>
2021-06-20 21:49:00 +0300

    jee mongo.js testailua

--- mongo.js
+++ mongo.js
@@ -2,9 +2,14 @@ const fs = require("fs");
 const secrets = JSON.parse(fs.readFileSync("secrets.json"));
 const password = secrets.dbpassword;
 const username = secrets.dbuser;
-const url = `mongodb+srv:${username}:${password}//cluster0.ojph5.mongodb.net/myFirstDatabase`;
+const url = `mongodb+srv://${username}:${password}@cluster0.ojph5.mongodb.net`;
 
+console.log(url);
+
+//const url = `mongodb+srv://cluster0.ojph5.mongodb.net/myFirstDatabase" --username ${username} --password${password}`;
 const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
+const uniqueValidator = require("mongoose-unique-validator");
 
 mongoose.connect(url, {
   useNewUrlParser: true,
@@ -15,15 +20,16 @@ mongoose.connect(url, {
 
 const personSchema = new mongoose.Schema({
   name: String,
-  number: String,
-  id: Number
+  number: String
 });
+personSchema.plugin(AutoIncrement, { inc_field: "id" });
+personSchema.plugin(uniqueValidator);
 
 const Person = mongoose.model("Person", personSchema);
 
 const person = new Person({
-  name: "Teppo Tulppu",
-  number: "123 1212 123",
+  name: process.argv[0],
+  number: process.argv[1],
   id: 1
 });
 
